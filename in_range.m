1de6c44760d33ccc8e80cd81abfe68b047290e5e
Eryk Sajur <eryk.sajur@gmail.com>
2017-11-12 02:34:54 +0100

    Add cubic spline interpolation

--- in_range.m
+++ in_range.m
@@ -1,9 +1,7 @@
 function y = in_range(x, minimum, maximum)
 	if (x >= minimum && x <= maximum)
-        % sprintf('%f in <%f, %f>', x, minimum, maximum)
 		y =  1;
 	else
-		% sprintf('%f not in <%f, %f>', x, minimum, maximum)
 		y =  0;
 	end
 end
---
+++ spline_interpolation.m
@@ -0,0 +1,31 @@
+function yy = spline_interpolation(x, y, alpha, beta, xx)
+	% assuming x and y sizes are equal
+	n = size(x,2) - 1;
+	c_mat = c_matrix(n+1);
+	a = x(1);
+	b = x(n+1);
+	h = (b - a)/(n);
+
+	y(1) = y(1) + h/3 * alpha; y(n+1) = y(n+1) - h/3 * beta;
+
+	c = c_mat\y'; % c - coefficients
+	c = [
+		c(2) - h/3 * alpha; 
+		c; 
+		c(size(c,1)-1) + (h/3 * beta)
+	]
+	pause
+	yy = zeros(1, size(xx,2));
+	for i = 1:size(xx,2)
+		yy(i) = interpolate(xx(i), c, a, h, n);
+	end
+end
+
+function y = interpolate(x, c, a, h, n)
+	y = 0;
+	for j = -1:n+1% S(x) = sum(c(i) * phi(i, x)) for i from -1 to n+1
+		k = j+2;
+		y = y + c(k) * phi(j, x, a, h);
+	end
+	
+end
